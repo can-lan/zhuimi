fa2b13059693afb685c3e9d35038042fd754cc09
dong-can <37677502+dong-can@users.noreply.github.com>
2018-10-12 02:11:06 +0800

    Delete pool.js

--- pool.js
+++
@@ -1,9 +0,0 @@
-const mysql=require('mysql');
-const pool=mysql.createPool({
-    host:'127.0.0.1',
-    user:'root',
-    password:'',
-    database:'zm',
-    connectionLimit:10
-})
-module.exports=pool;
