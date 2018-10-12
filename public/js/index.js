7feadc219a69423ee411ad6a9bdcb2e814cb6765
dong-can <2292722428@qq.com>
2018-10-12 23:01:17 +0800

    a

--- public/js/index.js
+++ public/js/index.js
@@ -1,3 +1,73 @@
+//1--轮播图1
+  var timer=setInterval(function(){ 
+    vm.count++ 
+    if(vm.count==5) vm.count=0
+  },6000);
+  var vm=new Vue({
+    el:"#carrousel",
+    data:{
+      imgs:[
+        ["img/index/banner4-lg.jpg","img/index/banner4-sm.png"],//←无效,被视频替代
+        ["img/index/banner1-lg.jpg","img/index/banner1-sm.png"],
+        ["img/index/banner2-lg.jpg","img/index/banner2-sm.png"],
+        ["img/index/banner3-lg.jpg","img/index/banner3-sm.png"],
+        ["img/index/banner4-lg.jpg","img/index/banner4-sm.png"]
+        ],
+      count:0
+    },
+    methods:{
+      carrousel(i){ 
+        vm.count=i
+        clearInterval(timer);
+        timer=setInterval(function(){ 
+          vm.count++ 
+          if(vm.count==5) vm.count=0
+        },6000);
+       }
+    }
+  });
+
+//2--box控制
+  var as=document.querySelectorAll("[data-toggle='tab']");
+  for(var a of as){
+      a.onclick=function(){
+        var a=this;
+        var as=document.querySelectorAll("[data-toggle='tab']");
+        for(var b of as){
+            b.className="";
+        }
+        a.className="visible";
+        var id=a.getAttribute("data-target");
+        var box=document.querySelector(id);
+        var boxs=box.parentNode.parentNode.children;
+        if(box.className=="") {
+            box.className = "hidden";
+            a.className="";
+        }else{
+            for(var b of boxs){
+                if(b.tagName=="DIV"){
+                    var c=b.children;
+                    for(var d of c){
+                        d.className="hidden";
+                    }
+                }
+            }
+            box.className="";
+        }
+      }
+  }
+
+//3--轮播图2
+  function banBefore(){
+    document.getElementsByClassName("before")[0].parentElement.children[0].style.marginLeft="0%";
+  }
+  function banNext(){
+    document.getElementsByClassName("next")[0].parentElement.children[0].style.marginLeft="-100%";
+  }
+
+/*
+ *废弃--->第一版jquery--->请求替换导航栏
+ *第二版Vue请求导航栏---在header.js
 (async function(){
     var res=await ajax({
         url:"http://localhost:7000/index/",
@@ -32,3 +102,4 @@
     //将html插入导航栏.nav2
     document.querySelector(".nav2").innerHTML=html1;
 })()
+*/
