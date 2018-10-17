e30f89a28f72abd95928ecd4607ee581b4ade622
dong-can <2292722428@qq.com>
2018-10-17 20:19:32 +0800

    10/17 20:19

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express=require("express");
 const bodyParser=require("body-parser");
-const index=require("./routes/index.js");
+const index=require("./routes/index");
+const api=require("aliyun-apisign");
 
 const app=express();
 app.listen(7000);
@@ -8,3 +9,4 @@ app.listen(7000);
 app.use(bodyParser.urlencoded({extended:false}));
 app.use(express.static('public'));
 app.use("/index",index);
+app.use("/api",api);
