53f76349f2fb5fceb647ca52611fcbd35e80f853
dong-can <2292722428@qq.com>
2018-10-27 21:43:27 +0800

    10/27 21:42 domain.html

--- public/js/domain.js
+++ public/js/domain.js
@@ -1,9 +1,34 @@
 $(function(){
-  (async function(){
+  /*(async function(){
     var res=await $ajax({
-      url:`https://cnz.co/domain-registration/domain.php?action=caajax&domain_name=${domain}`,
+      url:``,
       type:'get'
     })
     console.log(res);
-  })
+  })*/
+
+  new Vue({
+    el:"#data",
+    data:function(){
+      return {
+        ban:0
+      }
+    },
+    methods:{
+
+    },
+    created(){
+      setInterval(()=>{
+        var wrapper=$(".ban ul.wrapper")[0];
+        this.ban++;
+        $(wrapper).css("marginLeft",this.ban * -283);
+        if(this.ban==3){
+          $(wrapper).css("marginLeft","-283");
+          this.ban=1;
+        };
+      },2000)
+    }
+  });
+  //录播图.ban
+  
 })
